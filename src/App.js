48999408742ece5ff7ed498918edc9f75055185a
Emma Lynn <emmalynn@Emmas-Mac-mini.local>
2021-03-29 13:25:30 -0700

    update content

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ function App() {
     <div className="App">
       <Nav/>
       <div style={{padding:20,width:'100%',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
-        <Message sender content='by yung jake Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
-        <Message content='current auctions'/>
-        <img className='emoji-image' src={punk11} alt="punk 11"/>
-        <Message content='emoji punks 1 short description short description short description LINK TO OPEN SEA'/>
-        <img className='emoji-image' src={punk12} alt="punk 12"/>
-        <Message sender content='emoji punks 2 short description short description short description LINK TO OPEN SEA'/>
         <Message content='new yungjake project?'/>
         <Message sender content='emoji punks 2 short description short description short description LINK TO OPEN SEA'/>
         <Message sender content='yea, shitty emojisyungjake is bringing his art to the hands of his fans and new collectors, with a project called “shitty emojis”   https://opensea.io/collection/shitty-emojis'/>
