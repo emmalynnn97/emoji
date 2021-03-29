6d2d7b52e031c6e6efcd463766421ba847170c48
Emma Lynn <emmalynn@Emmas-Mac-mini.local>
2021-03-29 11:19:46 -0700

    add more content and make footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './Nav'
 import Message from './Message'
 import punk11 from './punk11.png'
 import Footer from './Footer'
+import punk12 from './punk12.png'
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,13 @@ function App() {
         <Message sender content='by yung jake Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
         <Message content='current auctions'/>
         <img src={punk11} alt="punk 11"/>
+        <Message content='emoji punks 1
+short description short description short description LINK TO OPEN SEA'/>
+        <img src={punk12} alt="punk 12"/>
+        <Message sender content='emoji punks 2
+short description short description short description
+LINK TO OPEN SEA
+'/>
         <Footer/>
       </div>
       
--- src/Footer.js
+++ src/Footer.js
@@ -3,13 +3,27 @@ import React from 'react'
 const Footer = () => {
     const footerStyle={
         width:288,
-        height:76,
+        height:50,
         position:'fixed',
-        bottom:0
+        bottom:0,
+        background:'rgb(237, 236, 242)',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'flex-end',
+    }
+    const btnStyle={
+        textTransform:'uppercase',
+        border:'2px solid rgb(0,111,255)',
+        color:'#006FFF',
+        padding:'.35em 1em',
+        borderRadius:'4px',
+        fontSize:13
     }
     return (
         <div style={footerStyle}>
-            Footer
+            <a style={btnStyle}>Buy</a>
+            <a style={btnStyle}>Discord</a>
+            <a style={btnStyle}>Twitter</a>
         </div>
     )
 }
